9eb443c24ad5046f13dd88310456719d0c1d0dc6
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-27 11:15:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
-  get '/' do
-    erb :index
+   get '/' do
+    erb :user_input
   end
 
-  post '/' do
-  @analyzed_text = TextAnalyzer.new(params[:user_text])
- 
-  erb :results
-end
+  post '/piglatinize' do
+    @text = params[:user_phrase]
+    @piglatinizer = PigLatinizer.new
+    erb :piglatinize
+  end
 
 end
